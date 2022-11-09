1342aed6858d6ecf169974bc04a9ad4a2f994915
leslie <sarybell28@gmail.com>
2022-11-09 00:05:25 -0500

    Post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,15 +13,26 @@
 
         
     </head>
-    <body class="bg-gray-100 text gray">
-        @foreach ($posts as $post )
-        <div>
-            <h2>{{$post->title}}</h2>
-        </div>
+    <body class="bg-gray-100 text-gray-700">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-3 my-10">
+                 @foreach ($posts as $post )
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{$post->title}}</h2>
+                    <p class="text-xs ">{{$post->excerpt}}</p>
+                    <p class="text-xs text-right">{{$post->published_at}}</p>
+                </div>
             
-        @endforeach
+                 @endforeach
+            </div>
+            <div class="mb-10">
+                 {{$posts->links()}}
+            </div>
+           
+        </div>
+       
 
-        {{$posts->links()}}
+        
        
     </body>
 </html>
