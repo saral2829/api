92bd66fc54b962604821c39fae983b43b65362c1
leslie <sarybell28@gmail.com>
2022-11-08 23:43:45 -0500

    tailwild

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,11 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-       
+       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         
     </head>
-    <body>
+    <body class="bg-gray-100 text gray">
         @foreach ($posts as $post )
         <div>
             <h2>{{$post->title}}</h2>
